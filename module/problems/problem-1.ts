f9115aba77ab664324e57cab92b8a6cef8ef930a
theabsparrow <absbashar04@gmail.com>
2024-11-09 18:44:53 +0600

    problem one complete

--- module/problems/problem-1.ts
+++ module/problems/problem-1.ts
@@ -1,2 +1,12 @@
-const course: string ="next level"
-console.log(course)
+
+function sumArray(numArr: number[]): number {
+    let sum: number = 0;
+    for (let i = 0; i < numArr.length; i++) {
+        sum += numArr[i];
+    }
+    return sum;
+}
+
+const numbers: number[] = [1, 41, 3, 20, 5];
+const result: number = sumArray(numbers);
+console.log(result);
